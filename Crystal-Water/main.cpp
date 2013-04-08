d32ff51471f9159093430d9fb4d76e6296c711ff
Wade Burch <wade@wadeburch.com>
2013-04-08 17:23:14 -0500

    Layout complete.

--- Crystal-Water/main.cpp
+++ Crystal-Water/main.cpp
@@ -1,15 +1,18 @@
 /*
  * main.cpp
  *
- *  Created on: Apr 8, 2013
- *      Author: nolnoch
+ *    Created on: Apr 8, 2013
+ *   Last Update: Apr 8, 2013
+ *  Orig. Author: Wade Burch (nolnoch@cs.utexas.edu)
+ *  Contributors: <none>
  */
 
 #include <GL/glew.h>
-#include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <cstdlib>
 #include <cmath>
 #include <cstring>
+#include <iostream>
 
 #include "./quaternion.h"
 
@@ -27,7 +30,17 @@
 // Materials
 
 
+// Window
+const GLint WIN_WIDTH = 800;
+const GLint WIN_HEIGHT = 600;
+
 // Input
+
+
+
+/*********************************
+ * Function Stubs
+ */
 
 
 
@@ -44,26 +57,75 @@ void Display() {
  * Interaction
  */
 
-void MouseClick() {
+void MouseClick(int button, int state, int x, int y) {
 
 }
 
-void MouseMotion() {
+void MouseMotion(int x, int y) {
 
 }
 
-void Keyboard() {
+void MouseWheel(int wheel, int direction, int x, int y) {
 
 }
+
+void Keyboard(unsigned char key, int x, int y) {
+
+}
+
+void Idle() {
+
+}
+
 
 /*********************************
  * Init Functions
  */
 
-void Init() {
+void BufferInit() {
 
 }
 
-int main(int argc, char** argv) {
+void ShaderInit() {
 
+}
+
+void OpenGLInit() {
+
+}
+
+int main(int argc, char* argv[]) {
+  // Initialize freeglut
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
+  glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+  glutInitWindowPosition(50, 50);
+
+  glutCreateWindow("Crystal-Water");
+  glutDisplayFunc(Display);
+  glutMouseFunc(MouseClick);
+  glutMotionFunc(MouseMotion);
+  glutMouseWheelFunc(MouseWheel);
+  glutKeyboardFunc(Keyboard);
+  glutIdleFunc(Idle);
+
+  // Initialize GLEW
+  glewExperimental = true;
+  if (glewInit() != GLEW_OK) {
+    cout << "GLEW initialization failed. Aborting program..." << endl;
+    return -1;
+  }
+
+  // Initialize OpenGL state
+  OpenGLInit();
+
+  // Initialize GLSL program
+  ShaderInit();
+
+  // Initialize buffer/array objects
+  BufferInit();
+
+  glutMainLoop();
+
+  return 0;
 }
