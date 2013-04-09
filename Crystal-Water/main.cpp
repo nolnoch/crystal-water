4737417a179972d1bf37a02bf364e3990a48d8fa
Wade Burch <wade@wadeburch.com>
2013-04-09 04:11:19 -0500

    Set up manual matrix maintenance structure.

--- Crystal-Water/main.cpp
+++ Crystal-Water/main.cpp
@@ -10,6 +10,8 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <cstdlib>
 #include <cmath>
 #include <cstring>
@@ -27,14 +29,18 @@
 Program progShader;
 
 // Lighting
-
-
-// Materials
-
+glm::vec4 light_ambient(0.1f, 0.1f, 0.1f, 1.0f);
+glm::vec4 light_diffuse(1.0f, 1.0f, 1.0f, 1.0f);
+glm::vec4 light_specular(1.0f, 1.0f, 1.0f, 1.0f);
+glm::vec4 light_position(40, 40, 40, 1.0f);
 
 // Window
 const GLint WIN_WIDTH = 800;
 const GLint WIN_HEIGHT = 600;
+
+// View and Transformation
+GLfloat fovy, aspect, zNear, zFar;
+glm::mat4 mModel, mProj, mRot;
 
 // Input
 
@@ -44,6 +50,16 @@ const GLint WIN_HEIGHT = 600;
  * Function Stubs
  */
 
+void Display();
+void DrawSomething();
+void MouseClick(int button, int state, int x, int y);
+void MouseMotion(int x, int y);
+void MouseWheel(int wheel, int direction, int x, int y);
+void Keyboard(unsigned char key, int x, int y);
+void Idle();
+void BufferInit();
+void ShaderInit();
+void OpenGLInit();
 
 
 /*********************************
@@ -51,7 +67,29 @@ const GLint WIN_HEIGHT = 600;
  */
 
 void Display() {
+  glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
+  DrawSomething();
+
+  glFlush();
+  glutSwapBuffers();
+}
+
+
+/*********************************
+ * Rendering
+ */
+
+void DrawSomething() {
+  progShader.enable();
+
+  progShader.setUniformMatrix(4, "rotationMatrix", glm::value_ptr(mRot));
+  progShader.setUniformMatrix(4, "modelviewMatrix", glm::value_ptr(mModel));
+  progShader.setUniformMatrix(4, "projectionMatrix", glm::value_ptr(mProj));
+
+  // Render here.
+
+  progShader.disable();
 }
 
 
@@ -85,17 +123,52 @@ void Idle() {
  */
 
 void BufferInit() {
-
+  // Fill when we have data to send to buffer.
 }
 
 void ShaderInit() {
+  // Add Shaders
   progShader.addDefaultShaders();
+
+  // Init Program object
   progShader.init();
+
+  // Bind Attributes
+  // progShader.bindAttribute(0, "someAttribute");
+
+  // Link and Validate program
   progShader.linkAndValidate();
+
+  // Generate Sampler2D uniform binding target
+  // progShader.addSampler();
 }
 
 void OpenGLInit() {
+  // Depth Testing
+  glEnable(GL_DEPTH_TEST);
+  glDepthMask(GL_TRUE);
+  glDepthFunc(GL_LEQUAL);
 
+  // Blending
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  // Clear Color
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+  // Scale Normalization
+  glEnable(GL_NORMALIZE);
+  glEnable(GL_RESCALE_NORMAL);
+
+  //Variables
+  fovy = 40.0f;
+  aspect = WIN_WIDTH / static_cast<float>(WIN_HEIGHT);
+  zNear = 1.0f;
+  zFar = 800.0f;
+
+  // Matrices
+  mProj = glm::perspective(fovy, aspect, zNear, zFar);
+  mModel = glm::mat4(1.0);
+  mRot = glm::mat4(1.0);
 }
 
 int main(int argc, char* argv[]) {
